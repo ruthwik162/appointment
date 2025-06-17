1af8e158a716ce97e21b0cbd958baafc721301f8
M.Nagaruthwik <nagaruthwikmerugu162@gmail.com>
2025-06-17 18:29:27 +0530

    fotter updated

--- src/Commmon-Components/Footer.jsx
+++ src/Commmon-Components/Footer.jsx
@@ -1,65 +1,77 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { FiHeart, FiMail, FiPhone } from 'react-icons/fi';
+
 
 const Footer = () => {
     return (
         <div className="w-full p-2 overflow-x-hidden">
             <footer className="p-5 rounded-2xl md:px-10 lg:px-16 w-full text-gray-900 bg-gray-100 dark:bg-gray-600 dark:text-gray-100">
-                <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-800 pb-10">
+                <div className="flex flex-col md:flex-row justify-between w-full gap-5 md:gap-10 border-b border-gray-800 pb-10">
                     {/* Left Section */}
-                    <div className="md:max-w-md">
-                        <img
-                            className="w-36 h-auto"
-                            src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoWhite.svg"
-                            alt="logo"
-                        />
-                        <p className="mt-6 text-sm text-gray-800 dark:text-gray-200">
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
-                        </p>
-                        <div className="flex items-center gap-2 mt-4">
+                    <div className="max-w-sm md:max-w-md w-full">
+                        {/* Logo and Description */}
+                        <div className="flex flex-row sm:flex-row items-center sm:items-start gap-4 sm:gap-6">
+                            <img
+                                className="w-24 h-auto rounded-2xl"
+                                src={assets.logo}
+                                alt="logo"
+                            />
+                            <p className="text-sm text-black text-center sm:text-left px-2 sm:px-0">
+                                This project is a modern student-teacher appointment booking system designed to simplify and digitize academic scheduling.
+                            </p>
+                        </div>
+
+                        {/* Store Buttons */}
+                        <div className="flex justify-center sm:justify-start items-center gap-3 mt-4">
                             <img
                                 src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/googlePlayBtnBlack.svg"
-                                alt="google play"
+                                alt="Google Play"
                                 className="h-10 w-auto border border-white rounded"
                             />
                             <img
                                 src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/appleStoreBtnBlack.svg"
-                                alt="app store"
+                                alt="App Store"
                                 className="h-10 w-auto border border-white rounded"
                             />
                         </div>
                     </div>
 
+
                     {/* Right Section */}
-                    <div className="flex-1 flex flex-col sm:flex-row md:justify-end gap-10 md:gap-20">
+                    <div className="flex-1 flex flex-col sm:flex-row md:justify-end gap-2 md:gap-20">
                         <div className='flex flex-row'>
-                          <div>
-                            <h2 className="font-semibold mb-5">Company</h2>
-                            <ul className="text-sm flex-col items-center justify-center space-y-2">
-                                <li><Link to="#">Home</Link></li>
-                                <li><Link to="#">About us</Link></li>
-                                <li><Link to="#">Contact us</Link></li>
-                                <li><Link to="#">Privacy policy</Link></li>
-                            </ul>
-                        </div>
-                        <div>
-                          <h2 className="font-semibold mb-5">Get in touch</h2>
-                            <div className="text-sm space-y-2">
-                                <p>+1-234-567-890</p>
-                                <p>contact@example.com</p>
+                            <div className='md:block hidden'>
+                                <h2 className="font-semibold mb-5">Company</h2>
+                                <ul className="text-sm flex-col items-center justify-center space-y-2">
+                                    <li><Link to="#">Home</Link></li>
+                                    <li><Link to="#">About us</Link></li>
+                                    <li><Link to="#">Contact us</Link></li>
+                                    <li><Link to="#">Privacy policy</Link></li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h2 className="font-semibold mb-1">Get in touch</h2>
+                                <div className="flex text-black items-center gap-2">
+                                    <FiPhone className="text-[#06d6a0] text-lg" />
+                                    <p>+1-234-567-890</p>
+                                </div>
+
+                                <div className="flex items-center gap-2">
+                                    <FiMail className="text-[#06d6a0] text-lg" />
+                                    <p className='text-black'>contact@example.com</p>
+                                </div>
                             </div>
                         </div>
-                        </div>
 
                         <div>
-                            
-
-                            <div className='pt-5'>
+                            <div className='md:block hidden'>
                                 <p className='text-lg text-gray-600 dark:text-gray-300'>STAY UPDATED</p>
-                                <p className='mt-3 text-sm'>
+                                <p className='mt-3  text-sm'>
                                     Subscribe to our newsletter for inspiration and special offers.
                                 </p>
-                                <div className='flex items-center mt-4 max-w-full sm:max-w-xs'>
+                                <div className=' flex items-center  mt-4 max-w-full sm:max-w-xs'>
                                     <input
                                         type="text"
                                         className='flex-grow bg-gray-200 rounded-l border border-gray-300 h-9 px-3 outline-none'
@@ -76,9 +88,17 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <p className="pt-4 text-center text-sm pb-5">
-                    © {new Date().getFullYear()} Appointment. All Rights Reserved.
-                </p>
+                <div className="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-10 px-4 py-4 text-black text-center">
+                    <p className="text-sm sm:text-[3.5vw] md:text-sm leading-snug">
+                        © {new Date().getFullYear()} Appointment. All Rights Reserved.
+                    </p>
+
+                    <div className="flex items-center shadow-2xl gap-2 text-sm sm:text-[3.5vw] md:text-sm">
+                        <p>Made with</p>
+                        <FiHeart className="text-green-500" />
+                        <p>By Nagaruthwik</p>
+                    </div>
+                </div>
             </footer>
         </div>
     )
--- src/Student-Pages/Home.jsx
+++ src/Student-Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Mainbanner from '../student-Components/Mainbanner'
 import DeptCat from '../student-Components/DeptCat'
 import Testimonial from '../student-Components/Testimonial'
+import About from '../Commmon-Components/About'
 
 const Home = () => {
     return (
         <div className='bg-gradient-to-r  from-blue-500/10 via-teal-500/10 to-green-500/10'>
             <Mainbanner />
             <Testimonial/>
+            <About/>
             <DeptCat />
         </div>
     )
